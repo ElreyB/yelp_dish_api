997ab8c044bfa1eb150b41d15aa6dc0e3d4958cc
Elrey Belmonti and Megan Olson <elrey+megan@>
2017-12-14 08:34:28 -0800

    create reviews table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171214162943) do
+ActiveRecord::Schema.define(version: 20171214163251) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,11 @@ ActiveRecord::Schema.define(version: 20171214162943) do
     t.string "dish"
     t.string "drink"
     t.integer "rating"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "content"
+    t.string "author"
   end
 
 end
