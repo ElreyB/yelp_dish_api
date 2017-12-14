f5bdf3685e3d05043e327bab1a9a924cdfa74fca
Elrey Belmonti and Megan Olson <elrey+megan@>
2017-12-14 10:24:09 -0800

    Add test for put meal request.

---
+++ spec/requests/put_meal_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper' do
+
+
+  describe "put a destination route", :type => :request do
+
+    describe do
+      @meal = FactoryBot.create(:meal)
+      @meal.reload
+    end
+
+    it "retunrs a sucess response" do
+      put "/api/v1/meals/#{@meal.id}", params: {:dish => "New Dish"}, headers: {Authorization: 'Basic bmFtZTpwYXNzd29yZA=='}
+      expect(response).to have_http_status(200)
+    end
+
+  end
+
+
+end
